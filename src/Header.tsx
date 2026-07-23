56178f8a38fe695cd8f504ce89a2688b3469e7f8
valentin makovsky <megavalentin09091972@gmail.com>
2026-07-23 15:56:09 +0300

    4.1 update

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,34 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useFavoritesStore } from './store/useFavoritesStore';
-import {useCartStore} from './store/useCartStore'
+import { useCartStore } from './store/useCartStore';
 import styles from './styles/App.module.css'; 
-import './index.css'
-
+import './index.css';
+import { Heart,ShoppingCart } from 'lucide-react';
 
 const Header: React.FC = () => {
   const { favorites } = useFavoritesStore();
-  const {cart} = useCartStore();
+  const { cart } = useCartStore();
   const favoriteItems = Object.values(favorites);
+  
   return (
     <header className={styles.header}>
-         <Link to = '/' className={styles.title}>
-          <h1>byresale</h1>
-          </Link>
+      <Link to='/'>
+        <h1 className={styles.title}>byresale</h1>
+      </Link>
+      
       <nav className={styles.nav}>
         <Link to="/favorites" className={styles.navLink}>
-          Избранное 
+          <Heart/>
           {favoriteItems.length > 0 && (
             <span className={styles.badge}>{favoriteItems.length}</span>
           )}
-        <Link to = '/cart' className={styles.navLink}>
-          Корзина
+        </Link> 
+
+        <Link to='/cart' className={styles.navLink}>
+          <ShoppingCart/>
           {Object.keys(cart).length > 0 && (
-           <span className={styles.badge}>{Object.keys(cart).length}</span>
-           )}
-          {}
-        </Link>
-          
+            <span className={styles.badge}>{Object.keys(cart).length}</span>
+          )}
         </Link>
       </nav>
     </header>
